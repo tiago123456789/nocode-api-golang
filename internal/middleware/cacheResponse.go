82daa2f1a8b348ac76aadae17fe9ff7ce4d2c169
Tiago R. da costa <tiagorosadacost@gmail.com>
2025-02-24 20:44:47 -0300

    Improve organization of code

--- internal/middleware/cacheResponse.go
+++ internal/middleware/cacheResponse.go
@@ -1,27 +1,19 @@
 package middleware
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/redis/go-redis/v9"
 	"github.com/tiago123456789/nocode-api-golang/internal/config"
 	"github.com/tiago123456789/nocode-api-golang/internal/types"
+	"github.com/tiago123456789/nocode-api-golang/internal/utils"
 )
 
 func CacheResponse(cache *redis.Client) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		extraCacheKey := ""
 		endpoint := c.Locals(c.Path()).(types.Endpoint)
-		if len(endpoint.QueryParams) > 0 {
-			for _, key := range endpoint.QueryParams {
-				value := c.Query(key)
-				extraCacheKey += fmt.Sprintf("%s_%s", key, value)
-			}
-		}
-
-		cacheKey := fmt.Sprintf("response_cached_%s_%s", c.Path(), extraCacheKey)
+		cacheKey := utils.GetCacheKeyByEndpoint(endpoint, c)
 		dataCached, _ := cache.Get(config.GetCacheContext(), cacheKey).Result()
 		if dataCached != "" && len(dataCached) > 0 {
 			c.Set("content-type", "application/json; charset=utf-8")
