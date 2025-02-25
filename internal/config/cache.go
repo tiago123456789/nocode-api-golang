bf4fc75d9b3ab06fecaa4c6f701f14ab25198d21
Tiago R. da costa <tiagorosadacost@gmail.com>
2025-02-25 19:51:15 -0300

    enabled lru on redis to remove least recently used key from cache

--- internal/config/cache.go
+++ internal/config/cache.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/redis/go-redis/v9"
@@ -9,6 +10,7 @@ import (
 
 var ctx = context.Background()
 var rdb *redis.Client
+var logger = GetLogger()
 
 func GetCacheContext() context.Context {
 	return ctx
@@ -20,6 +22,22 @@ func InitCache() {
 		Password: os.Getenv("REDIS_PASSWORD"),
 		DB:       0,
 	})
+
+	err := rdb.ConfigSet(
+		context.Background(),
+		"maxmemory",
+		os.Getenv("REDIS_LIMIT_MEMORY"),
+	).Err()
+	if err != nil {
+		logger.Error(fmt.Sprintf("Failed to set maxmemory: %v", err))
+		panic(err)
+	}
+
+	err = rdb.ConfigSet(context.Background(), "maxmemory-policy", "allkeys-lru").Err()
+	if err != nil {
+		logger.Error(fmt.Sprintf("Failed to set LRU policy: %v", err))
+		panic(err)
+	}
 }
 
 func GetCache() *redis.Client {
